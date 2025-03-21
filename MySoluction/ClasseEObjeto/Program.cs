930d4cb8bf05b9064d61a06e690f7f4fc839bc37
D3v-DGomes <devgomes1223@hotmail.com>
2025-03-21 05:35:45 -0300

    Aprendendo C#

--- MySoluction/ClasseEObjeto/Program.cs
+++ MySoluction/ClasseEObjeto/Program.cs
@@ -52,37 +52,81 @@ Corpo do método -> Consiste das instruções definidas entre chaves no corpo do
 
 
 
-var aluno = new Aluno();
+// var aluno = new Aluno();
 
-Console.WriteLine("Nome : ");
-aluno.Nome = Console.ReadLine();
-Console.WriteLine("Idade : ");
-aluno.Idade = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Sexo : ");
-aluno.Sexo = Console.ReadLine();
-Console.WriteLine("Aprovado (S)im : (N)ão");
-aluno.Aprovado = Console.ReadLine();
+// Console.WriteLine("Nome : ");
+// aluno.Nome = Console.ReadLine();
+// Console.WriteLine("Idade : ");
+// aluno.Idade = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Sexo : ");
+// aluno.Sexo = Console.ReadLine();
+// Console.WriteLine("Aprovado (S)im : (N)ão");
+// aluno.Aprovado = Console.ReadLine();
 
-Curso curso = new();
-curso.Resultado(aluno);
+// Curso curso = new();
+// curso.Resultado(aluno);
 
-public class Aluno {
-    public string? Nome;
-    public int Idade;
-    public string? Sexo;
-    public string? Aprovado;
-}
+// public class Aluno {
+//     public string? Nome;
+//     public int Idade;
+//     public string? Sexo;
+//     public string? Aprovado;
+// }
 
 
-public class Curso {
-    public void Resultado(Aluno aluno){
-        Console.WriteLine($"\nO aluno {aluno.Nome}, sexo {aluno.Sexo} com {aluno.Idade} anos.");
-        if(aluno.Aprovado == "S") {
-            Console.WriteLine("\nFoi Aprovado.");
-        } else {
-            Console.WriteLine($"\n foi Reprovado.");
-        }
+// public class Curso {
+//     public void Resultado(Aluno aluno){
+//         Console.WriteLine($"\nO aluno {aluno.Nome}, sexo {aluno.Sexo} com {aluno.Idade} anos.");
+//         if(aluno.Aprovado == "S") {
+//             Console.WriteLine("\nFoi Aprovado.");
+//         } else {
+//             Console.WriteLine($"\n foi Reprovado.");
+//         }
+//     }
+
+// }
+
+
+
+
+// CONSTRUTOR PADRÃO
+//  public class Aluno2 {      // class com construtor automático
+//     public string? Name;
+//     public int Age;
+//     public string? Sex;
+//  }
+
+// CONSTRUTOR PADRÃO COM PARÂMETROS
+
+// Homem homem = new Homem("Jorge", 29, "masculino");
+
+// public class Homem {
+//     public Homem(string name, int age, string sex){
+//         Name = name;
+//         Age = age;
+//         Sex = sex;
+//     }
+
+//     public string Name;
+//     public int Age;
+//     public string Sex;
+// }
+
+// CHAMANDO UM CONSTRUTOR DENTRO DE OUTRO
+Person person = new("João", 32, "masculino");
+Console.WriteLine(person.Name);
+Console.WriteLine(person.Age);
+Console.WriteLine(person.Sex);
+
+public class Person {
+    public Person(string name) => Name = name;
+
+    public Person(string name, int age, string sex):this(name) {
+        Age = age;
+        Sex = sex;
     }
 
+    public string? Name;
+    public int Age;
+    public string Sex;
 }
-
