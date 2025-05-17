8b3834a60aaa9d9f97be7e02f9f89b36ce163ea8
D3v-DGomes <devgomes1223@hotmail.com>
2025-05-17 07:59:29 -0300

    adding class about exceptions - Microsoft Learn

---
+++ MySoluction/MicrosoftLearn/aula015.4/Program.cs
@@ -0,0 +1,100 @@
+﻿/* Create an exception object */
+
+// Configure and throw custom exceptions:
+
+// ArgumentException invalidArgumentException = new ArgumentException("ArgumentException: The 'GraphData' method received data outside the expected range.");
+// throw invalidArgumentException;
+
+
+// throw new FormatException("FormatException: Calculations in process XYZ have been cancelled due to invalid data format.");
+
+
+// When to throw an exception:
+/*
+Consider a scenario where a developer is working on an application that implements a business process. 
+The business process depends on user input. If the input does not match the expected data type, 
+the method that implements the business process creates and throws an exception. 
+The exception object can be configured with application-specific information in the property values. 
+The following code example demonstrates the scenario:
+*/
+
+try
+{
+    OperatingProcedure1();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Exiting application.");
+}
+
+static void OperatingProcedure1()
+{
+    string[][] userEnteredValues = new string[][]
+    {
+        new string[] { "1", "two", "3"},
+        new string[] { "0", "1", "2"}
+    };
+
+    foreach(string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            BusinessProcess1(userEntries);
+        }
+        catch (Exception ex)
+        {
+            if (ex.StackTrace.Contains("BusinessProcess1"))
+            {
+                if (ex is FormatException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Corrective action taken in OperatingProcedure1");
+                }
+                else if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+                    // re-throw the original exception
+                    throw;
+                }
+                else
+                {
+                    // create a new exception object that wraps the original exception
+                    throw new ApplicationException("An error occurred - ", ex);
+                }
+            }
+        }
+
+    }
+}
+
+static void BusinessProcess1(string[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        try
+        {
+            valueEntered = int.Parse(userValue);
+
+            checked
+            {
+                int calculatedValue = 4 / valueEntered;
+            }
+        }
+        catch (FormatException)
+        {
+            FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+            throw invalidFormatException;
+        }
+        catch (DivideByZeroException)
+        {
+            DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+            throw unexpectedDivideByZeroException;
+
+        }
+    }
+}
